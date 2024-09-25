8611c377d0b166672878ef3f07409ff3f411f58e
cenglevs <st087271@student.spbu.ru>
2024-09-25 12:40:36 +0300

    Create GetPostsForUser fucntion that retreives all posts that user follows

--- main.go
+++ main.go
@@ -70,6 +70,7 @@ func main() {
 	v1Router.Post("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerCreateFeedFollow))
 	v1Router.Get("/feed_follows", apiCfg.middlewareAuth(apiCfg.handlerGetFeedFollows))
 	v1Router.Delete("/feed_follows/{feedFollowID}", apiCfg.middlewareAuth(apiCfg.handlerDeleteFeedFollow))
+	v1Router.Get("/posts", apiCfg.middlewareAuth(apiCfg.handlerGetPostsForUser))
 
 	router.Mount("/v1", v1Router) // For maintaining different versions
 
